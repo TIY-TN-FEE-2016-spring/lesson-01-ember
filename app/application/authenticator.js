2db483fd350ddba14985b52712169e4764ef2c5c
Ryan Tablada <ryan.tablada@gmail.com>
2016-04-26 12:41:44 -0500

    Filter by user id

--- app/application/authenticator.js
+++ app/application/authenticator.js
@@ -2,5 +2,5 @@ import Loopback from 'ember-simple-auth-loopback/authenticators/loopback';
 import config from 'lesson-01-ember/config/environment';
 
 export default Loopback.extend({
-  loginEndpoint: `${config.apiHost}/api/end-users/login`,
+  loginEndpoint: `${config.apiHost}/api/end-users/login?include=end-user`,
 });
--- app/dashboard/my-recipes/route.js
+++ app/dashboard/my-recipes/route.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  session: Ember.inject.service(`session`),
+
+  model() {
+    // Get user id from the session
+    const userId = this.get(`session.data.authenticated.userId`);
+
+    // Filter where user id is the current user id
+    return this.store.query(`recipe`, {
+      filter: {
+        where: {
+          'user-id': userId,
+        },
+      },
+    });
+  },
 });
